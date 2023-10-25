6a266a9145551e2ad87a20308db0782cfff12f00
anshulisinghal <144349360+anshulisinghal@users.noreply.github.com>
2023-10-25 12:57:47 +0530

    Add files via upload

---
+++ Movie_booking_sql/Screen.sql
@@ -0,0 +1,32 @@
+/*
+-- Query: SELECT * FROM Movie_Booking.Screen
+LIMIT 0, 1000
+
+-- Date: 2023-10-25 12:57
+*/
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (1,'Screen 1',20,1);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (2,'Screen 2',15,1);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (3,'Hall 1',25,2);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (4,'Screen 1',20,3);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (5,'Screen 2',30,3);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (6,'Screen 3',30,3);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (7,'Audi 1',200,4);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (8,'Audi 2',250,4);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (9,'Audi 3',300,4);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (10,'Audi 4',175,4);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (11,'Screen 1',200,5);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (12,'Screen 2',250,5);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (13,'Hall 1',225,6);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (14,'Hall 2',225,6);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (15,'Screen 1',250,7);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (16,'Screen 2',200,7);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (17,'Audi 1',200,8);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (18,'Audi 2',150,8);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (19,'Audi 3',300,8);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (20,'Audi 4',250,8);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (21,'Screen 1',200,9);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (22,'Screen 2',250,9);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (23,'Screen 1',150,10);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (24,'Screen 2',200,10);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (25,'Screen 3',200,10);
+INSERT INTO `` (`id`,`Name`,`Total_Seats`,`Theater_ID`) VALUES (26,'Screen 4',250,10);
